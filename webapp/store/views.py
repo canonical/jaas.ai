d8ef373debb1d973a43a92cc9ea0d5d1ce8a2357
Anthony Dillon <me@anthonydillon.com>
2018-08-16 15:38:15 +0100

    Integrate theblues and load entity data

--- webapp/store/views.py
+++ webapp/store/views.py
@@ -1,9 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, abort, current_app
+from theblues.charmstore import CharmStore
+from pprint import pformat
 
 
 jaasstore = Blueprint(
   'jaasstore', __name__,
   template_folder='/templates', static_folder='/static')
+
+cs = CharmStore("https://api.jujucharms.com/v5")
 
 
 @jaasstore.route('/store')
@@ -11,6 +15,12 @@ def store():
     return render_template('store/store.html')
 
 
-@jaasstore.route('/<charm_name>')
-def details(charm_name):
-    return render_template('store/details.html')
+@jaasstore.route('/<entity_name>')
+def details(entity_name):
+    try:
+        entity = cs.entity(entity_name)
+        entity_formatted = pformat(entity, 1, 120)
+    except Exception:
+        return abort(404, "Entity not found {}".format(entity_name))
+
+    return render_template('store/details.html', entity=entity_formatted)
