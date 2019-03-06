2133d7fa2322016fc09f0865b54da043f5c55880
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-03-06 13:21:53 +0000

    Add global-nav

--- static/js/src/app.js
+++ static/js/src/app.js
@@ -1,5 +1,7 @@
 // import modules
 import showContactDetails from './modules/show-contact-details';
+import {createNav} from 'global-nav';
 
 // Instantiate modules
 showContactDetails();
+createNav();
