959726cac40b3b0fbce76ae95ea3d23c3f438f9f
Huw Wilkins <huwshimi@gmail.com>
2019-03-14 14:13:28 +1100

    Add search form to the header.

--- static/js/src/app.js
+++ static/js/src/app.js
@@ -1,5 +1,6 @@
 // import modules
 import {createNav} from '@canonical/global-nav';
+import handleSearch from './modules/header-search';
 import showContactDetails from './modules/show-contact-details';
 import copySnippet from './modules/copy-snippet';
 
@@ -7,3 +8,4 @@ import copySnippet from './modules/copy-snippet';
 createNav({maxWidth: '64.875rem', showLogins: false});
 showContactDetails();
 copySnippet();
+handleSearch();
