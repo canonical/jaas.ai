d4bbf260e32f694b3c305594dc2f8fbe18086f4f
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-03-20 17:03:01 +0000

    Update view.py

--- webapp/store/views.py
+++ webapp/store/views.py
@@ -5,11 +5,11 @@ from jujubundlelib import references
 
 
 jaasstore = Blueprint(
-  'jaasstore', __name__,
-  template_folder='/templates', static_folder='/static')
+    "jaasstore", __name__, template_folder="/templates", static_folder="/static"
+)
 
 
-@jaasstore.route('/store')
+@jaasstore.route("/store")
 def store():
     return render_template('store/store.html')
 
@@ -66,50 +66,55 @@ def search_redirect(path=None):
 @jaasstore.route('/u/<username>/')
 def user_details(username):
     entities = models.get_user_entities(username)
-    if len(entities['charms']) > 0 or len(entities['bundles']) > 0:
+    if len(entities["charms"]) > 0 or len(entities["bundles"]) > 0:
         return render_template(
-            'store/user-details.html',
+            "store/user-details.html",
             context={
-                'bundles_count': len(entities['bundles']),
-                'bundles': entities['bundles'],
-                'charms_count': len(entities['charms']),
-                'charms': entities['charms'],
-                'entities': entities,
-                'username': username
-            }
+                "bundles_count": len(entities["bundles"]),
+                "bundles": entities["bundles"],
+                "charms_count": len(entities["charms"]),
+                "charms": entities["charms"],
+                "entities": entities,
+                "username": username,
+            },
         )
     else:
         return abort(404, "User not found: {}".format(username))
 
 
-@jaasstore.route('/u/<username>/<charm_or_bundle_name>')
-@jaasstore.route(
-    '/u/<username>/<charm_or_bundle_name>/<series_or_version>')
-@jaasstore.route(
-    '/u/<username>/<charm_or_bundle_name>/<series_or_version>/<version>')
-def user_entity(username, charm_or_bundle_name, series_or_version=None,
-                version=None):
-    return details(charm_or_bundle_name, series_or_version=series_or_version,
-                   version=version)
+@jaasstore.route("/u/<username>/<entity_name>/")
+def user_entity(username, entity_name):
+    reference = references.Reference.from_jujucharms_url(request.path[1:])
+    charm_or_bundle = models.get_charm_or_bundle(reference)
+
+    if charm_or_bundle:
+        if charm_or_bundle["is_charm"]:
+            return render_template(
+                "store/charm-details.html", context={"charm": charm_or_bundle}
+            )
+        else:
+            return render_template(
+                "store/bundle-details.html", context={"bundle": charm_or_bundle}
+            )
+    else:
+        return abort(404, "Entity not found {}".format())
 
 
-@jaasstore.route('/<charm_or_bundle_name>')
-@jaasstore.route('/<charm_or_bundle_name>/<series_or_version>')
-@jaasstore.route('/<charm_or_bundle_name>/<series_or_version>/<version>')
+@jaasstore.route("/<charm_or_bundle_name>")
+@jaasstore.route("/<charm_or_bundle_name>/<series_or_version>")
+@jaasstore.route("/<charm_or_bundle_name>/<series_or_version>/<version>")
 def details(charm_or_bundle_name, series_or_version=None, version=None):
     reference = references.Reference.from_jujucharms_url(request.path[1:])
     charm_or_bundle = models.get_charm_or_bundle(reference)
 
     if charm_or_bundle:
-        if charm_or_bundle['is_charm']:
+        if charm_or_bundle["is_charm"]:
             return render_template(
-                'store/charm-details.html',
-                context={'charm': charm_or_bundle}
+                "store/charm-details.html", context={"charm": charm_or_bundle}
             )
         else:
             return render_template(
-                'store/bundle-details.html',
-                context={'bundle': charm_or_bundle}
+                "store/bundle-details.html", context={"bundle": charm_or_bundle}
             )
     else:
         return abort(404, "Entity not found {}".format(charm_or_bundle_name))
