f7062ae21f5575c35415cff6c4091ca191c71e98
Huw Wilkins <huwshimi@gmail.com>
2019-03-22 09:36:07 +1100

    Add filtering and sorting.

--- webapp/store/views.py
+++ webapp/store/views.py
@@ -16,11 +16,29 @@ def store():
 
 @jaasstore.route('/search/')
 def search():
-    query = request.args.get('q').replace('/', ' ')
-    results = models.search_entities(query)
+    query = request.args.get('q', '').replace('/', ' ')
+    entity_type = request.args.get('type', None)
+    if entity_type not in ['charm', 'bundle']:
+        entity_type = None
+    series = request.args.get('series', None)
+    tags = request.args.get('tags', None)
+    sort = request.args.get('sort', None)
+    provides = request.args.get('provides', None)
+    requires = request.args.get('requires', None)
+    if provides:
+        results = models.fetch_provides(provides)
+    elif requires:
+        results = models.fetch_requires(requires)
+    else:
+        results = models.search_entities(query, entity_type=entity_type, tags=tags,
+                                     sort=sort, series=series,
+                                     promulgated_only=False)
     return render_template(
         'store/search.html',
         context={
+            'current_series': series,
+            'current_sort': sort,
+            'current_type': entity_type,
             'results': results,
             'results_count':
                 len(results['recommended']) + len(results['community']),
