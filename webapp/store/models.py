e2930f38d362846639b31e46f70cdc53f590b334
Huw Wilkins <huwshimi@gmail.com>
2019-04-08 09:45:06 +1000

    Replace CDK entity names.

--- webapp/store/models.py
+++ webapp/store/models.py
@@ -267,7 +267,12 @@ def _get_display_name(name):
         :param name the charm/bundle name.
         :return a cleaned name for display.
     """
-    return name.replace("-", " ")
+    name = name.replace("-", " ")
+    # Hack to rename 'canonical kubernetes'. To be removed when display
+    # name has been implemented.
+    if name == 'canonical kubernetes':
+        name = 'the charmed distribution of kubernetes'
+    return name
 
 
 def _get_bug_url(name, bugs_url):
