e0bd8bb0b36846cd239ae1c863d0ed5fedbaa427
Huw Wilkins <huwshimi@gmail.com>
2019-04-11 11:55:28 +1000

    Add a json feed of the blog posts.

--- tests/jaasai/test_views.py
+++ tests/jaasai/test_views.py
@@ -1,5 +1,7 @@
+import json
 from flask import url_for
 from flask_testing import TestCase
+from unittest.mock import patch, MagicMock
 
 from webapp.app import create_app
 
@@ -37,3 +39,41 @@ class WebappViews(TestCase):
             self.assertEqual(
                 response.status_code, 200, "For page: {}".format(url)
             )
+
+    @patch("feedparser.parse")
+    def test_blog_feed(self, mock_parse):
+        mock_parse.return_value = MagicMock(
+            bozo=0, entries=[{"title": "A blog post"}]
+        )
+        response = self.client.get(url_for("jaasai.blog_feed"))
+        self.assertEqual(
+            response.data, b'[\n  {\n    "title": "A blog post"\n  }\n]\n'
+        )
+
+    @patch("feedparser.parse")
+    def test_blog_feed_only_5(self, mock_parse):
+        mock_parse.return_value = MagicMock(
+            bozo=0,
+            entries=[
+                {"title": 1},
+                {"title": 2},
+                {"title": 3},
+                {"title": 4},
+                {"title": 5},
+                {"title": 6},
+            ],
+        )
+        response = self.client.get(url_for("jaasai.blog_feed"))
+        self.assertEqual(len(json.loads(response.data)), 5)
+
+    @patch("feedparser.parse")
+    def test_blog_feed_invalid(self, mock_parse):
+        feed = MagicMock(
+            bozo=1,
+            bozo_exception=MagicMock(
+                getMessage=MagicMock(return_value="Syntax error")
+            ),
+        )
+        mock_parse.return_value = feed
+        response = self.client.get(url_for("jaasai.blog_feed"))
+        self.assertEqual(response.data, b'{\n  "error": "Syntax error"\n}\n')
