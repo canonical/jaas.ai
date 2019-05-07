197718e08a6cfc7cf13d6ec0a8729196728c0f69
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-05-08 00:00:59 +0100

    Add cookie-policy
    Fixes #213

--- static/js/src/app.js
+++ static/js/src/app.js
@@ -1,5 +1,6 @@
 // import modules
 import {createNav} from '@canonical/global-nav';
+import {cookiePolicy} from '@canonical/cookie-policy';
 import searchPanel from './modules/search-panel';
 import showContactDetails from './modules/show-contact-details';
 import copySnippet from './modules/copy-snippet';
@@ -9,3 +10,10 @@ createNav({maxWidth: '72rem', showLogins: false});
 showContactDetails();
 copySnippet();
 searchPanel();
+
+const cookiePolicyOptions = {
+  content: `We use cookies to improve your experience. By your continued use of this site you accept such use. To change your settings please <a href="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy#cookies">see our policy.</a>`,
+  duration: 3000
+};
+
+cookiePolicy(cookiePolicyOptions);
