f1007d413add0554681c0212c6a83eca907e5d84
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-05-22 10:53:49 +0100

    Add Sentry JS tracking

--- static/js/src/app.js
+++ static/js/src/app.js
@@ -1,9 +1,13 @@
 // import modules
+import * as Sentry from '@sentry/browser';
 import {createNav} from '@canonical/global-nav';
 import {cookiePolicy} from '@canonical/cookie-policy';
 import searchPanel from './modules/search-panel';
 import showContactDetails from './modules/show-contact-details';
 import copySnippet from './modules/copy-snippet';
+
+// Init Sentry JS tracking
+Sentry.init({dsn: 'https://5b54e6946be34749935c4dd2d9d01cb8@sentry.is.canonical.com//7'});
 
 // Global nav strip
 createNav({maxWidth: '72rem', showLogins: false});
