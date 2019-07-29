ad2db2c6fdb81853e4e31b5564939f06b380e0fd
Barry McGee <barry.mcgee@integralcode.co.uk>
2019-07-29 16:11:21 +0100

    Update docs link

--- webapp/external_urls.py
+++ webapp/external_urls.py
@@ -3,7 +3,6 @@ external_urls = {
     "blog": "https://blog.ubuntu.com/topics/juju",
     "charmstore": "https://api.jujucharms.com/charmstore/v5/",
     "discourse": "https://discourse.jujucharms.com/",
-    "docs": "https://docs.jujucharms.com/",
     "gui": "https://jujucharms.com/new/",
     "issues": "https://github.com/canonical-web-and-design/jaas.ai/issues",
 }
