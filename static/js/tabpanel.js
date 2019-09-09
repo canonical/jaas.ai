9fd7ffcc2fafae563f75a4f9dfd7e1263cc15365
Anthony Dillon <me@anthonydillon.com>
2019-09-09 21:20:22 +0100

    Add animation to the homepage tabs

--- static/js/tabpanel.js
+++ static/js/tabpanel.js
@@ -1,3 +1,5 @@
+let timer;
+
 window.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('[role="tab"]');
 
@@ -5,9 +7,16 @@ window.addEventListener('DOMContentLoaded', () => {
   tabs.forEach(tab => {
     tab.addEventListener('click', changeTabs);
   });
+
+  // Select the inital active tab
+  const initalActiveTab = document.querySelector('.p-hero-tab__item[aria-selected="true"]');
+  if (initalActiveTab) {
+    playTab(initalActiveTab);
+  }
 });
 
 function changeTabs(e) {
+  clearInterval(timer);
   const target = e.target.closest('[role="tab"]');
   const tabs = document.querySelectorAll('[role="tab"]');
   const panels = document.querySelectorAll('[role="tabpanel"]');
@@ -25,4 +34,40 @@ function changeTabs(e) {
 
   // Show the selected panel
   document.querySelector(`#${target.getAttribute('aria-controls')}`).removeAttribute('hidden');
+
+  playTab(target);
+}
+
+function playTab(tab) {
+  let start = Date.now();
+  let duration = 10000;
+  let tabIndicator = tab.querySelector('.before');
+  if (tabIndicator) {
+    setTimeout(function() {
+      timer = setInterval(function() {
+        let timePassed = Date.now() - start;
+        if (timePassed >= duration) {
+          clearInterval(timer);
+          triggerNextTab(tab);
+          return;
+        }
+        draw(timePassed / (duration / 100), tabIndicator);
+      }, 20);
+    }, 500);
+  }
+}
+
+function draw(timePassed, tab) {
+  tab.style.width = timePassed + '%';
+}
+
+function triggerNextTab(tab) {
+  setTimeout(function() {
+    draw(0, tab.querySelector('.before'));
+    let nextTab = tab.nextElementSibling;
+    if (!nextTab) {
+      nextTab = document.querySelectorAll('.p-hero-tab__item')[0];
+    }
+    nextTab.click();
+  }, 1000);
 }
