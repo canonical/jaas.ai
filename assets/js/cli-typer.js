43a50d6bb1609d5c8c15f7871178a884c607317c
Anthony Dillon <me@anthonydillon.com>
2020-01-13 13:47:23 +0000

    Move the static directory to assets

---
+++ assets/js/cli-typer.js
@@ -0,0 +1,219 @@
+/**
+  Initialize the typer
+  @param {HTMLElement} container The main container to type in
+  @param {HTMLElement} commentContainer the container to add comments
+*/
+window.app.cliTyper = function(container, commentContainer) {
+  var cli = [
+    'juju clouds',
+    'juju add-credential aws-personal',
+    'juju add-credential aws-work',
+    'juju bootstrap aws',
+    'juju status',
+    'juju deploy mariadb',
+    'juju deploy mariadb --constraints cores=8 mem=16G',
+    'juju config mariadb dataset-size=80%',
+    'juju add-model prod-california us-west-1',
+    'juju add-model prod-tokyo ap-northeast-1',
+    'juju list-models',
+    'juju deploy jenkins',
+    'juju deploy',
+    'juju deploy jenkins-slave',
+    'juju relate jenkins jenkins-slave',
+    'juju add-unit jenkins-slave -n 3',
+    'juju add-user sarah',
+    'juju grant sarah read prod-california',
+    'juju add-cloud mystack mystack-config.yaml'
+  ];
+
+  // For future decorations of the CLI
+  var comments = [
+    'Choose a cloud to work with',
+    'Juju manages multiple cloud credentials',
+    false,
+    "Let's set up a controller on a public cloud",
+    "What's happening?",
+    'Juju deploys applications with a single command',
+    'Need more power?',
+    'Tune the application',
+    'Build and manage many models',
+    false,
+    false,
+    'Want continuous integration?',
+    false,
+    false,
+    'Configure your applications to work together. Juju charms handle connections',
+    'Increased demand? Bring up 3 more nodes...',
+    'Juju is multi-user',
+    false,
+    'Use your own private cloud - including OpenStack'
+  ];
+
+  var RESET_SPEED = 1000;
+  var BASE_SPEED = 50;
+  var SPEED_MODIFIER = 100;
+  var DELETE_PAUSE = 250;
+  var NEXT_PAUSE = 2000;
+
+  var cliEle = container;
+  var commentEle = commentContainer;
+  var current = '';
+
+  /**
+    Get the difference between two strings
+
+    @param {String} orig The source string
+    @param {String} next The target string
+  */
+  function getDiff(orig, next) {
+    var changes = [];
+    var matchTo = 0;
+    var toAdd = [];
+    var i = 0;
+
+    for (i = orig.length; i > -1; i -= 1) {
+      if (next[i] !== orig[i]) {
+        matchTo = i;
+      }
+    }
+
+    if (matchTo > 0) {
+      for (i = 0; i < orig.length - matchTo; i += 1) {
+        changes.push(-1);
+      }
+      changes.push(DELETE_PAUSE);
+    }
+
+    i = matchTo;
+    for (var ii = next.length; i < ii; i += 1) {
+      toAdd.push(next[i]);
+    }
+
+    changes = changes.concat(toAdd);
+
+    changes.push(NEXT_PAUSE);
+
+    return changes;
+  }
+
+  /**
+    Add a comment based on the decorator
+
+    @param {Boolean} hide whether the element should be hidden
+    @param {HTMLElement} _comment the string to display
+    @param {Function} cb Callback if waiting for transition
+  */
+  function comment(hide, _comment, cb) {
+    if (!hide) {
+      commentEle.innerHTML = _comment;
+      commentEle.classList.add('fade-in');
+    } else {
+      commentEle.classList.remove('fade-in');
+    }
+
+    setTimeout(cb, 500);
+  }
+
+  /**
+    Type out all the changes in order with delays and randomness
+    to simulate someone typing.
+
+    @param {Array} changes The change list (get's modified)
+    @param {Array} original The original change list
+  */
+  function typeIt(changes, original) {
+    var change = changes.shift();
+    var speed = BASE_SPEED + Math.ceil(Math.random() * SPEED_MODIFIER);
+    var currentText = current;
+    if (typeof change === 'string' && change.indexOf('#') === 0) {
+      comment(true, false, function() {
+        comment(false, change.substr(1));
+      });
+    } else if (change === -1) {
+      currentText = currentText.substr(0, current.length - 1);
+      speed = BASE_SPEED;
+    } else if (typeof change === 'number' && change >= 0) {
+      speed = change;
+    } else {
+      currentText = currentText + change;
+    }
+
+    var html = '<span>' + currentText.split(' ').join('</span> <span>') + '</span>';
+
+    current = currentText;
+
+    cliEle.innerHTML = html;
+
+    var next = (function() {
+      var _changes = changes;
+
+      return function() {
+        typeIt(_changes, original);
+      };
+    })();
+
+    if (changes.length > 0) {
+      setTimeout(next, speed);
+    } else {
+      typeIt(original.slice(0), original);
+    }
+  }
+
+  /**
+    Get the changes in an array.
+
+    @param {Array} arr the array
+    @param {Boolean} resetAtEnd Start again?
+  */
+  function changesInArray(arr, resetAtEnd) {
+    var changes = [];
+    var i;
+    var current;
+    var previous;
+
+    for (i = 0, ii = arr.length; i < ii; i += 1) {
+      current = arr[i];
+      previous = arr[i - 1] || '';
+
+      changes.push(getDiff(previous, current));
+    }
+
+    if (resetAtEnd) {
+      var reset = [];
+      for (i = 0, ii = current.length; i < ii; i += 1) {
+        reset.push(-1);
+      }
+      reset.push(RESET_SPEED);
+      changes.push(reset);
+    }
+
+    return changes;
+  }
+
+  /**
+    Flatten a multidimensional array
+
+    @param {Array} arr the original array
+  */
+  function flattenArray(arr) {
+    var newArr = [];
+
+    for (var i = 0, ii = arr.length; i < ii; i += 1) {
+      newArr = newArr.concat(arr[i]);
+    }
+
+    return newArr;
+  }
+
+  var changeList = changesInArray(cli, true);
+
+  // Decorate with comments
+  for (var i = 0, ii = changeList.length; i < ii; i += 1) {
+    if (comments[i]) {
+      changeList[i].unshift('#' + comments[i]);
+    }
+  }
+
+  var changes = flattenArray(changeList);
+  typeIt(changes.slice(0), changes);
+};
---
+++ assets/js/treefilelist.js
@@ -0,0 +1,103 @@
+/**
+  Converts a DOM list by splitting the text by '/' into a nested tree list.
+**/
+window.app.treeifyFileList = () => {
+  /**
+    Loop through the list a files passing the file title and children.
+    Skipping the rootAttribute object.
+
+    @method treeifyFileList.addList
+    @param folderContents {Object} Object representation of a file path
+    @static
+  **/
+  function addList(folderContents) {
+    for (var key in folderContents) {
+      if (key !== rootAttribute) {
+        addItem(key, folderContents[key]);
+      }
+    }
+  }
+
+  var files = {},
+    DOMList = document.querySelector('#files .p-list-tree'),
+    rootAttribute = '_root',
+    renderedList = '',
+    fileUrl = '';
+
+  if (DOMList) {
+    fileUrl = DOMList.querySelector('a').href;
+    fileUrl = fileUrl.substr(0, fileUrl.indexOf('/archive/') + 9);
+
+    DOMList.querySelectorAll('li').forEach(e => parsePath(e.innerText.trim()));
+    addList(files);
+    DOMList.innerHTML = renderedList;
+
+    var listTreeToggle = document.querySelectorAll('.p-list-tree__toggle');
+    for (var i = 0; i < listTreeToggle.length; i++) {
+      listTreeToggle[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        var listTree = this.nextElementSibling;
+        var expand = this.getAttribute('aria-expanded') === 'true' ? false : true;
+        this.setAttribute('aria-expanded', expand);
+        listTree.setAttribute('aria-hidden', !expand);
+      });
+    }
+  }
+
+  /**
+    Splits a path string by '/' into a multidimensional object.
+
+    @method treeifyFileList.parsePath
+    @param fileURL {String} Path string
+    @static
+  **/
+  function parsePath(fileURL) {
+    var cur = files;
+    fileURL.split('/').forEach(function(node) {
+      cur[node] = cur[node] || {};
+      cur[node][rootAttribute] = fileURL;
+      cur = cur[node];
+    });
+  }
+
+  /**
+    Appends the appoporiate markup based on a node or leaf element.
+
+    @method treeifyFileList.addItem
+    @param key {String} node title
+    @param folder {Object} nodes object
+    @static
+  **/
+  function addItem(key, folder) {
+    if (isEmpty(folder)) {
+      renderedList += '<li class="p-list-tree__item">';
+      renderedList +=
+        '<a href="' + fileUrl + folder[rootAttribute] + '" target="_blank">' + key + '</a>';
+      renderedList += '</li>';
+    } else {
+      renderedList +=
+        '<li class="p-list-tree__item p-list-tree__item--group"><button class="p-list-tree__toggle" role="tab" aria-controls="sub-1" aria-expanded="false">/' +
+        key +
+        '</button>';
+      renderedList += '<ul class="p-list-tree"></li>';
+      addList(folder);
+      renderedList += '</ul>';
+    }
+  }
+
+  /**
+    Tests the passed object for contents other then a path object.
+
+    @method treeifyFileList.isEmpty
+    @param obj {Object} test object for emptyness
+    @static
+  **/
+  function isEmpty(obj) {
+    for (var prop in obj) {
+      if (prop !== rootAttribute && obj.hasOwnProperty(prop)) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
