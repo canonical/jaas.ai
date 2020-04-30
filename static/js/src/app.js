e2c494bff5b90b5f4a3bf120f5ad08d48cd17e96
Barry McGee <barry.mcgee@integralcode.co.uk>
2020-04-30 15:12:53 +0100

    Refactor modals JS to a module

--- static/js/src/app.js
+++ static/js/src/app.js
@@ -5,6 +5,7 @@ import {cookiePolicy} from '@canonical/cookie-policy';
 import searchPanel from './modules/search-panel';
 import showContactDetails from './modules/show-contact-details';
 import copySnippet from './modules/copy-snippet';
+import modal from './modules/modal';
 
 // Init Sentry JS tracking
 Sentry.init({dsn: 'https://5b54e6946be34749935c4dd2d9d01cb8@sentry.is.canonical.com//7'});
@@ -20,6 +21,9 @@ copySnippet();
 
 // Display search panel overlay when user clicks in nav search box
 searchPanel();
+
+// Show modal windows
+modal();
 
 // Add cookie policy on page load
 const cookiePolicyOptions = {
