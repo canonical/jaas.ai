1df43d4033c4d28205fe08a74b6f5576519fd66f
Anthony Dillon <me@anthonydillon.com>
2022-01-27 14:47:03 +0000

    Remove old search redirects

--- webapp/redirects/views.py
+++ webapp/redirects/views.py
@@ -9,22 +9,6 @@ jaasredirects = Blueprint(
 )
 
 
-@jaasredirects.route("/q/")
-@jaasredirects.route("/q/<path:path>")
-def search_redirect(path=None):
-    """
-    Handle redirects from jujucharms.com search URLS to the jaas.ai format.
-    e.g. /q/k8s/demo?sort=-name&series=xenial will redirect to
-    /search?q=k8s+demo&sort=-name&series=xenial
-    """
-    query_string = []
-    if path:
-        query_string.append("q={}".format(path.replace("/", "+")))
-    if request.query_string:
-        query_string.append(str(request.query_string, "utf-8"))
-    return redirect("/search?{}".format("&".join(query_string)), code=302)
-
-
 @jaasredirects.route("/u/<username>/<charm_or_bundle_name>")
 @jaasredirects.route(
     "/u/<username>/<charm_or_bundle_name>/<series_or_version>"
